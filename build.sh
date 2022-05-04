65a7a9f3b97c3548bcecb9216b77342ea2b921a2
Ryan Qian <i@bitbili.net>
2022-05-04 18:19:41 +0800

    init commit
    Signed-off-by: Ryan Qian <i@bitbili.net>

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+
+_L="${1}"
+
+_P=${_L%%/}
+_P=${_P%/*}
+
+pushd "${_P}"
+
+yarn add bower
+
+cd "${_L}"
+
+npm install --production
+
+proxychains -q ../node_modules/bower/bin/bower install -p
+
+popd
+
+
+pushd $(realpath $0)
+git checkout main
+rm -rf node_modules www
+cp -a ${_L}/{node_modules,www/bower_components} ./
+mkdir www
+mv bower_components www
+popd
